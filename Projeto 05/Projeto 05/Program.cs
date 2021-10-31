63998727d6cd34b01c25b8465200ea4f271f29f0
FelipeECarvalho <Felipeemanuelc12@gmail.com>
2021-10-31 17:01:15 -0300

    Alterações no Projeto 5

--- Projeto 05/Projeto 05/Program.cs
+++ Projeto 05/Projeto 05/Program.cs
@@ -3,7 +3,17 @@
 namespace Projeto_05 {
 	class Program {
 		static void Main(string[] args) {
-			Console.WriteLine("Olá Mundo!");
+			Console.WriteLine("Frase do dia: ");
+			foreach (char palavra in "Olá Mundo!") {
+				Console.WriteLine(palavra);
+			}
+
+			DateTime hoje = DateTime.Now;
+			string today = hoje.ToString();
+			Console.WriteLine("Dia de hoje: ");
+			foreach (char word in today) {
+				Console.WriteLine(word);
+			}
 		}
 	}
 }
